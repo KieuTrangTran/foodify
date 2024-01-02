7d2da1f8fe78f5c515a04f1c9939d3414bb1ba4e
Kieu Trang Tran <k.trangtran2003@gmail.com>
2024-01-02 11:39:55 +0100

    Added TestConfiguration Test for deployment

--- src/test/java/com/example/demo/FoodifyApplicationTests.java
+++ src/test/java/com/example/demo/FoodifyApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestConfiguration.class)
 class FoodifyApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(){
 	}
 
 }
+
+
